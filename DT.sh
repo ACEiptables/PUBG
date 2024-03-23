b504907e987fff1fe567b9ec6ef1218770c50af7
ACEiptables <164225517+ACEiptables@users.noreply.github.com>
2024-03-23 12:39:34 +0800

    Add files via upload

--- DT.sh
+++ DT.sh
@@ -105,4 +105,4 @@ iptables -A OUTPUT -m owner --uid-owner=$uid -p udp --dport 17500 -j DROP
 iptables -A OUTPUT -m owner --uid-owner=$uid -p tcp --dport 17500 -j DROP
 iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp -d w.t3data.net -j ACCEPT
 echo -e "\033[5;46;42;37m            【 小叽猪保驾护航中✔ 】                 \033[0m"
-echo -e "\033[5;46;42;37m              【 建议再执行一遍 】                 \033[0m"
+echo -e "\033[5;46;42;37m              【 建议再执行一遍☜ 】                 \033[0m"
