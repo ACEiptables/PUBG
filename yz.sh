f1b989f37aafc55128dee775773aa0ea649c8ad5
ACEiptables <164225517+ACEiptables@users.noreply.github.com>
2024-03-28 05:05:40 +0800

    Update yz.sh

--- yz.sh
+++ yz.sh
@@ -1,5 +1,5 @@
-FILE=/storge/emulated/0/Android/data/org.immomo.tcn.info/cache/-6057727307591827235_97.jpg
-FILE=/storag/emulated/0/Android/data/org.immomo.tcn.info/cache/-6057727307591827235_99.jpg
+FILE=/storage/emulated/0/Android/data/org.immomo.tcn.info/cache/-6057727307591827235_97.jpg
+FILE=/storaag/emulated/0/Android/data/org.immomo.tcn.info/cache/-6057727307591827235_99.jpg
 if [ ! -f "$FILE" ]; then
     echo "你没有加入电报:https://t.me/xiaojizhuNB"
     am start -a android.intent.action.VIEW -d https://t.me/xiaojizhuNB
