0c5cac206cd5393c46cce90a442c30b566c83a58
ACEiptables <164225517+ACEiptables@users.noreply.github.com>
2024-03-28 17:31:53 +0800

    Update yz.sh

--- yz.sh
+++ yz.sh
@@ -1,10 +1,4 @@
-FILE=/storage/emulated/0/Android/data/org.immomo.tcn.info/cache/-6057727307591827235_97.jpg
-FILE=/storage/emulated/0/Android/data/org.immomo.tcn.info/cache/-6057727307591827235_99.jpg
-if [ ! -f "$FILE" ]; then
-    echo "你没有加入电报:https://t.me/xiaojizhuNB"
-    am start -a android.intent.action.VIEW -d https://t.me/xiaojizhuNB
-    exit
-fi
+
 echo "可以使用"
 
 
