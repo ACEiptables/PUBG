a70be4b4849c3e939a4efbbb8310bb874f99f20c
ACEiptables <164225517+ACEiptables@users.noreply.github.com>
2024-03-28 17:31:14 +0800

    Add files via upload

--- ZM.sh
+++ ZM.sh
@@ -1,35 +1,10 @@
-uid=`cat /data/system/packages.list | grep com.tencent.ig | awk '{print $2}'`
 
 
 
-echo -e "\033[5;46;42;37m            【 连接成功 】                 \033[0m"
-echo -e "\033[5;46;42;37m            【 桌面启动开启 】                 \033[0m"
+uid=`cat /data/system/packages.list | grep 18525 | awk '{print $2}'`
 
-
-
-
-
-
-
-
-
-echo -ne '                   \033[1;37m  □□□□□□□□□□0% \r'
-
-echo -ne '                   \033[1;31m  ■□□□□□□□□□10% \r'
-
-
-
-
-
-iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 17500 -j ACCEPT
-iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 443 -j ACCEPT
-iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 8085 -j ACCEPT
-iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 8086 -j ACCEPT
-iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 17500 -j ACCEPT
-iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 443 -j ACCEPT
-iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 8085 -j ACCEPT
-iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 8086 -j ACCEPT
-
+echo -e "\033[5;46;42;37m            【 启动前游戏开启✔ 】                 \033[0m"
+echo -e "\033[5;46;42;37m            【 自动启动游戏✔ 】                 \033[0m"
 iptables -I OUTPUT -d cloud.vmp.onezapp.com -j REJECT
 iptables -I INPUT -s cloud.vmp.onezapp.com -j REJECT
 
@@ -67,8 +42,17 @@ iptables -A OUTPUT -m owner --uid-owner=$uid -p tcp --dport 9081 -j DROP
 iptables -A OUTPUT -m owner --uid-owner=$uid -p tcp --dport 9995 -j DROP
 
 
-
-echo -ne '                   \033[1;32m  ■■■■■■■■■□90% \r'
-sleep 0.1
-echo -ne '                   \033[1;32m  ■■■■■■■■■■100% \r'
-sleep 1.5
+echo -e "\033[5;46;42;37m            【 开启成功✔ 】                 \033[0m"
+echo "自启动……"
+FILE=/storage/emulated/0/自启动
+if [ ! -f "$FILE" ]; then
+    echo -e "\033[47m  【 检测到没有设置自启动已为您关闭❌ 】 \033[0m"
+    
+    echo -e "\033[33m  开启自启动请在【/storage/emulated/0/】创建名字为 自启动【文件】\033[0m"
+sleep 0.3
+   
+    exit 1
+    exit
+fi
+echo "开始跳转PUBG✔"
+am start -n com.tencent.ig/com.epicgames.ue4.SplashActivity
