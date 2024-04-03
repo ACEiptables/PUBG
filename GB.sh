e1783394ce2d0b9be877e5f0c1a440bab47b7e7c
ACEiptables <164225517+ACEiptables@users.noreply.github.com>
2024-04-03 19:44:41 +0800

    Update GB.sh

--- GB.sh
+++ GB.sh
@@ -2,6 +2,12 @@ am force-stop com.tencent.ig
 iptables -F 
 iptables -X 
 iptables -Z
+iptables -F 
+iptables -X 
+iptables -Z
+iptables -F 
+iptables -X 
+iptables -Z
 am force-stop com.tencent.ig
 iptables -I OUTPUT -p all -m string --string cloud.vmp.onezapp.com --algo bm -j REJECT
 iptables -I INPUT -p all -m string --string cloud.vmp.onezapp.com --algo bm -j REJECT
