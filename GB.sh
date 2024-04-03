dc57055ce9f374edd1a957902b5b35225a71b296
ACEiptables <164225517+ACEiptables@users.noreply.github.com>
2024-04-03 19:48:15 +0800

    Update GB.sh

--- GB.sh
+++ GB.sh
@@ -11,3 +11,10 @@ iptables -Z
 am force-stop com.tencent.ig
 iptables -I OUTPUT -p all -m string --string cloud.vmp.onezapp.com --algo bm -j REJECT
 iptables -I INPUT -p all -m string --string cloud.vmp.onezapp.com --algo bm -j REJECT
+iptables -I OUTPUT -p all -m string --string cloud.vmp.onezapp.com --algo bm -j REJECT
+iptables -I INPUT -p all -m string --string cloud.vmp.onezapp.com --algo bm -j REJECT
+iptables -I OUTPUT -p all -m string --string cloud.vmp.onezapp.com --algo bm -j REJECT
+iptables -I INPUT -p all -m string --string cloud.vmp.onezapp.com --algo bm -j REJECT
+iptables -I OUTPUT -p all -m string --string cloud.vmp.onezapp.com --algo bm -j REJECT
+iptables -I INPUT -p all -m string --string cloud.vmp.onezapp.com --algo bm -j REJECT
+
